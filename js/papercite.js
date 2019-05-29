2151aed3734f0e27f92761aa23a8ba32f190a670
twak <twakelly@gmail.com>
2019-05-29 13:54:00 +0100

    misc bugs

--- js/papercite.js
+++ js/papercite.js
@@ -5,7 +5,19 @@ var $j = jQuery.noConflict();
 $j(document).ready(function() {
     // Toggle Single Bibtex entry
     $j('a.papercite_toggle').click(function() {
-	$j( "#" + $j(this).attr("id") + "_block" ).toggle();
+
+        shown = ! $j( "#" + $j(this).attr("id") + "_block" ).is(":visible");
+
+
+        // if ($j(this).attr("id").includes(""))
+        // $j( "#" + $j(this).attr("id") + "_block" ).toggle();
+
+        $j( ".papercite_bibtex" ).hide();
+
+        if (shown)
+	        $j( "#" + $j(this).attr("id") + "_block" ).show();// toggle();
+        else
+            $j( "#" + $j(this).attr("id") + "_block" ).hide();// toggle();
 	return false;
     });
 });
